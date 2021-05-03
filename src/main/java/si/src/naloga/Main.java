c1020ad99025f18d8bb7754133262021e446df62
j-dolenc <jurij1.dolenc@gmail.com>
2021-05-03 16:42:31 +0200

    Dodal funkcionalnost brez baze, in nekaj neuspesno poskusil z bazo

--- src/main/java/si/src/naloga/Main.java
+++ src/main/java/si/src/naloga/Main.java
@@ -1,6 +1,7 @@
 package si.src.naloga;
 
 import si.src.naloga.imenik.TelefonskiImenik;
+import si.src.naloga.kontakt.Kontakt;
 
 import java.util.Scanner;
 
@@ -20,47 +21,167 @@ public class Main {
             akcija = in.next();
 
             switch (akcija) {
-                case "1":
+                case "1": {
                     telefonskiImenik.izpisiVseKontakte();
                     break;
-                case "2":
-                    telefonskiImenik.dodajKontakt();
+                }
+                case "2": {
+                    Kontakt novo = new Kontakt();
+                    
+                    String test=in.nextLine();
+                    
+                    System.out.println("Dodaj kontakt v imenik!");
+                    novo.setId(-1);
+
+                    System.out.println("Ime: ");
+                    novo.setIme(in.nextLine());
+
+                    System.out.println("Priimek: ");
+                    novo.setPriimek(in.nextLine());
+
+                    System.out.println("Naslov: ");
+                    novo.setNaslov(in.nextLine());
+
+                    System.out.println("Elektronska posta: ");
+                    novo.setElektronskaPosta(in.nextLine());
+
+                    System.out.println("Telefon: ");
+                    novo.setTelefon(in.nextLine());
+                    
+                    System.out.println("Mobilni telefon: ");
+                    novo.setMobilniTelefon(in.nextLine());
+
+                    System.out.println("Opomba: ");
+                    novo.setOpomba(in.nextLine());
+
+                    telefonskiImenik.dodajKontakt(novo);
                     break;
-                case "3":
-                    telefonskiImenik.urediKontakt();
+                }
+                case "3": {
+                    telefonskiImenik.izpisiVseKontakte();
+                    System.out.print("Vpisite ID kontakta, ki ga zelite urediti:");
+                    int idKontakta = Integer.parseInt(in.next());
+                    System.out.println(idKontakta);
+
+                    Kontakt oseba = new Kontakt();
+                    oseba.setId(idKontakta);
+                    Kontakt k = new Kontakt();
+                    for(Kontakt kontakt : telefonskiImenik.getSeznam()){
+                        if(oseba.getId() == kontakt.getId()){
+                            oseba=kontakt;
+                            k=kontakt;
+                        }
+                    }
+
+
+                    String sprememba = "";
+                    izpisiUrejanje();
+                    while(!"0".equals(sprememba)){
+                        sprememba=in.next();
+                        if(sprememba.equals("0")){
+                            break;
+                        }
+                        switch(sprememba){
+                            case "1": {
+                                System.out.print("Novo ime:");
+                                oseba.setIme(in.next());
+                                break;
+                            }
+                            case "2": {
+                                System.out.print("Nov priimek:");
+                                oseba.setPriimek(in.next());
+                                break;
+                            }
+                            case "3": {
+                                System.out.print("Nov naslov:");
+                                oseba.setNaslov(in.next());
+                                break;
+                            }
+                            case "4": {
+                                System.out.print("Nova elektronska posta:");
+                                oseba.setElektronskaPosta(in.next());
+                                break;
+                            }
+                            case "5": {
+                                System.out.print("Nov telefon:");
+                                oseba.setTelefon(in.next());
+                                break;
+                            }
+                            case "6": {
+                                System.out.print("Nov mobilni telefon:");
+                                oseba.setMobilniTelefon(in.next());
+                                break;
+                            }
+                            case "7": {
+                                System.out.print("Nova opomba:");
+                                oseba.setOpomba(in.next());
+                                break;
+                            }
+                            case "0": {
+                                break;
+                            }
+                            default:{
+                                System.out.println("Napacna izbira!!!");
+                                break;
+                            }
+                        }
+                        izpisiUrejanje();
+                    }
+                    telefonskiImenik.urediKontakt(oseba);
+
+                    //System.out.println("Urejanje kontakta zakljuceno!");
+                    System.out.println(oseba.toString());
                     break;
-                case "4":
-                    telefonskiImenik.izbrisiKontaktPoId();
+                }
+                case "4": {
+                    System.out.print("Prosim napisite ID kontakta, ki ga zelite izbrisati:");
+                    int idKontakta = in.nextInt();
+                    telefonskiImenik.izbrisiKontaktPoId(idKontakta);
                     break;
-                case "5":
-                    telefonskiImenik.izbrisiKontaktPoId();
+                }
+                case "5": {
+                    System.out.print("Prosim napisite ID kontakta, ki ga zelite izpisati:");
+                    int idKontakta = in.nextInt();
+                    telefonskiImenik.izpisiKontaktZaId(idKontakta);
                     break;
-                case "6":
+                }
+                case "6": {
                     telefonskiImenik.izpisiSteviloKontaktov();
                     break;
-                case "7":
+                }
+                case "7": {
                     telefonskiImenik.serializirajSeznamKontaktov();
                     break;
-                case "8":
+                }
+                case "8": {
                     telefonskiImenik.naloziSerializiranSeznamKontakotv();
                     break;
-                case "9":
+                }
+                case "9": {
                     telefonskiImenik.izvoziPodatkeVCsvDatoteko();
                     break;
-                case "0":
+                }
+                case "0": {
                     System.exit(0);
                     break;
-                default:
-                    System.out.println("Napačna izbira!!!");
+                }
+                case "42":{
+                    System.out.println("Bravo, nasel si pot do skritega koticka, zdaj pa nazaj na delo.");
                     break;
+                }
+                default:{
+                    System.out.println("Napacna izbira!!!");
+                    break;
+                }
             }
 
             izpisiMenu();
         }
+        in.close();	
     }
 
     /**
-     * Uporabniku izpišemo menu
+     * Uporabniku izpisemo menu
      */
     public static void izpisiMenu() {
 
@@ -69,12 +190,12 @@ public class Main {
         System.out.println("Aplikacija telefonski imenik:");
         System.out.println("-----------------------------------");
         System.out.println("Akcije:");
-        System.out.println("1 - izpiši vse kontakte v imeniku");
+        System.out.println("1 - izpisi vse kontakte v imeniku");
         System.out.println("2 - dodaj kontakt v imenik");
-        System.out.println("3 - uredi obstoječi kontakt");
-        System.out.println("4 - briši kontakt po ID-ju");
-        System.out.println("5 - izpiši kontakt po ID-ju");
-        System.out.println("6 - izpiši število vseh kontaktov");
+        System.out.println("3 - uredi obstojeci kontakt");
+        System.out.println("4 - brisi kontakt po ID-ju");
+        System.out.println("5 - izpisi kontakt po ID-ju");
+        System.out.println("6 - izpisi stevilo vseh kontaktov");
         System.out.println("7 - Shrani kontakte na disk (serializacija)");
         System.out.println("8 - Preberi kontake iz serializirano datoteke");
         System.out.println("9 - Izvozi kontakte v csv");
@@ -85,4 +206,23 @@ public class Main {
 
 
     }
+    public static void izpisiUrejanje() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Urejanje kontakta");
+        System.out.println("-----------------------------------");
+        System.out.println("Akcije urejanja:");
+        System.out.println("1 - Ime");
+        System.out.println("2 - Priimek");
+        System.out.println("3 - Naslov");
+        System.out.println("4 - Elektronsla posta");
+        System.out.println("5 - Telefon");
+        System.out.println("6 - Mobilni telefon");
+        System.out.println("7 - Opomba");
+        System.out.println("");
+        System.out.println("0 - Izhod iz aplikacije");
+        System.out.println("----------------------------------");
+        System.out.println("Akcija: ");    
+    }
+    
 }
--- src/main/java/si/src/naloga/imenik/TelefonskiImenik.java
+++ src/main/java/si/src/naloga/imenik/TelefonskiImenik.java
@@ -6,79 +6,140 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class TelefonskiImenik {
 
     private List<Kontakt> seznamKontaktov;
 
-    public TelefonskiImenik() {
+    public List<Kontakt> getSeznam(){
+        return seznamKontaktov;
+    }
+
+    public TelefonskiImenik() /*throws ClassNotFoundException*/{
+        
         seznamKontaktov = new ArrayList<>();
+        // Connection conn= null;
+        // try {
+        //     Class.forName("org.sqlite.JDBC");
+        //     String url = "jdbc:sqlite:TelefonskiImenik.db";
+        //     conn = DriverManager.getConnection(url);
+        //     Statement statement = conn.createStatement();
+        //     statement.setQueryTimeout(30);
+            
+        // }
+        // catch(SQLException e){
+        //     System.err.println(e.getMessage());
+        // }
+        // finally {
+        //     try{
+        //         if(conn != null) conn.close();
+        //     }
+        //     catch(SQLException e){
+        //         System.err.println(e);
+        //     }
+        // }
     }
 
+    // public List<Kontatk> getTelefonskiImenik() {
+    //     return seznamKontaktov;
+    // }
     /**
-     * Metaoda izpiše vse kontakte
+     * Metoda izpise vse kontakte
      */
+
     public void izpisiVseKontakte() {
-        System.out.println("Metoda še ni implementirana");
+        for(Kontakt kontakt : seznamKontaktov){
+            System.out.println(kontakt.toString());
+        }
+        //System.out.println("Metoda se ni implementirana");
     }
 
     /**
-     * Metaoda doda nov kontakt v imenik
+     * Metoda doda nov kontakt v imenik
      *
-     * onemogočimo dodajanje dupliciranega kontakta
+     * onemogocimo dodajanje dupliciranega kontakta
      */
-    public void dodajKontakt() {
-        System.out.println("Metoda še ni implementirana");
+    public void dodajKontakt(Kontakt e) {
+        //Scanner sc = new Scanner(System.in);
+        seznamKontaktov.add(e);
+        //System.out.println("Metoda se ni implementirana");
     }
 
     /**
-     * Metoda popravi podatke na obstoječem kontaktu
-     * ID kontakta ni mogoče spreminjati
+     * Metoda popravi podatke na obstojecem kontaktu
+     * ID kontakta ni mogoce spreminjati
      */
-    public void urediKontakt() {
-        System.out.println("Metoda še ni implementirana");
+    public void urediKontakt(Kontakt e) {
+        //kontakt iz imenika je urejen v Main, tukaj bom dodal urejanje v bazi...
+        //System.out.println("Metoda se ni implementirana");
     }
 
     /**
      * Brisanje kontakta po ID-ju
      */
-    public void izbrisiKontaktPoId() {
-        System.out.println("Metoda še ni implementirana");
+    public void izbrisiKontaktPoId(int id) {
+        //System.out.println("Metoda se ni implementirana");
+        int indeks = 0;
+        for(Kontakt kontakt : seznamKontaktov){
+            if(kontakt.getId() == id){
+                System.out.println("Izbrisan kontakt: "+kontakt.toString());
+                break;
+            }
+            indeks++;
+        }
+        seznamKontaktov.remove(indeks);
     }
 
     /**
      * Izpis kontakta po ID-ju
      */
-    public void izpisiKontaktZaId() {
-        System.out.println("Metoda še ni implementirana");
+    public void izpisiKontaktZaId(int id) {
+        for(Kontakt kontakt : seznamKontaktov){
+            if(kontakt.getId() == id){
+                System.out.println(kontakt.toString());
+            }
+        }
+        //System.out.println("Metoda se ni implementirana");
     }
 
     /**
-     * Izpis kontakta po ID-ju
+     * Izpis stevila kontaktov
      */
     public void izpisiSteviloKontaktov() {
-        System.out.println("Metoda še ni implementirana");
+        int st = 0;
+        for(Kontakt kontakt : seznamKontaktov){
+            st++;
+        }
+        System.out.println("V tem imeniku imate shranjenih " + st + " kontaktov.");
+        //System.out.println("Metoda se ni implementirana");
     }
 
     /**
-     * Serializiraj seznam kontoktov na disk.
+     * Serializiraj seznam kontaktov na disk.
      * Ime datoteke naj bo "kontakti.ser"
      */
     public void serializirajSeznamKontaktov() {
-        System.out.println("Metoda še ni implementirana");
+        System.out.println("Metoda se ni implementirana");
     }
 
     /**
-     * Pereberi serializiran seznam kontakotv iz diska
+     * Pereberi serializiran seznam kontaktov iz diska
      */
     public void naloziSerializiranSeznamKontakotv() {
-        System.out.println("Metoda še ni implementirana");
+        System.out.println("Metoda se ni implementirana");
     }
 
     /**
-     * Izvozi seznam kontakov CSV datoteko.
+     * Izvozi seznam kontaktov v CSV datoteko.
      * Naj uporabnik sam izbere ime izhodne datoteke.
      */
     public void izvoziPodatkeVCsvDatoteko() {
-        System.out.println("Metoda še ni implementirana");
+        System.out.println("Metoda se ni implementirana");
     }
 }
